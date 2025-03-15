9ba9b6e5f1dad62611a286dade197c96fce03ac1
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2025-03-15 19:48:06 +0000

    Trying to fix git shenanigans

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use dioxus::prelude::*;
+use dioxus::{html::input, prelude::*};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const _HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 fn main() {
     dioxus::launch(App);
@@ -13,25 +13,50 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
+        document::Title {"Weather Application"}
+        document::Link {rel: "preconnect", href: "https://fonts.googleapis.com"}
+        document::Link {rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous"}
+        document::Link {href: "https://fonts.googleapis.com/css2?family=Doto:wght@100..900&display=swap", rel: "stylesheet"}
+        SecondPage {location:  "This" }
 
     }
 }
 
 #[component]
-pub fn Hero() -> Element {
+pub fn SecondPage(location: String) -> Element {
+    let location = "Accra";
+    let status = "Raining";
     rsx! {
         div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            class: "container",
+            h2 {id: "title", "Weather Application" }
+
+            img{
+                src: "https://cdn.weatherapi.com/weather/64x64/day/116.png"
             }
+
+            "{location}, {status}",
+            button {id:"button",  "Retry"}
         }
     }
 }
+
+#[component]
+pub fn FirstPage() -> Element {
+
+    rsx! {
+        div {
+            class: "container",
+            h2 {id: "title", "Weather Application" },
+            span {
+                input {type:"text", placeholder: "Location" , "What is your name"}
+                button { id: "next",  "Next" }
+              },
+            
+        }
+    }
+}
+
+
+
+
